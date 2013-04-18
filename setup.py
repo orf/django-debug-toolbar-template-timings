86bf65955311e3d654b7869d83fd722adfa86602
orf <tom@tomforb.es>
2013-04-18 17:26:42 +0100

    Fixed setup.py not including templates

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from distutils.core import setup
 
 setup(
     name='django-debug-toolbar-template-timings',
-    version='0.1',
+    version='0.2',
     packages=['template_timings_panel', 'template_timings_panel.panels'],
+    package_data={'': ['templates/*']},
     url='',
     license='',
     author='Tom Forbes',
     author_email='tom@tomforb.es',
-    description='A django-debug-toolbar panel that shows you template rendering times for Django'
+    description='A django-debug-toolbar panel that shows you template rendering times for Django',
+    requires=['Django', 'debug_toolbar'],
 )
