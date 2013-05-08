1e36b925445f7608d0ea324dee9a12b81c71c68c
Ben Tappin <ben@mrben.co.uk>
2013-05-08 08:28:21 +0100

    Add MIT license.

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from distutils.core import setup
 
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
 setup(
     name='django-debug-toolbar-template-timings',
     version='0.2',
     packages=['template_timings_panel', 'template_timings_panel.panels'],
     package_data={'': ['templates/*']},
     url='',
-    license='',
+    license=license,
     author='Tom Forbes',
     author_email='tom@tomforb.es',
     description='A django-debug-toolbar panel that shows you template rendering times for Django',
