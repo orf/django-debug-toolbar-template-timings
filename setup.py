ebf3760cdb72f08f115c3c248d1d473b0bbded65
orf <tom@tomforb.es>
2013-06-19 12:37:26 +0200

    Update setup.py
    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+import os
 
+license = ""
 
-with open('LICENSE') as f:
-    license = f.read()
+if os.path.isfile("LICENSE"):
+    with open('LICENSE') as f:
+        license = f.read()
 
 
 setup(
     name='django-debug-toolbar-template-timings',
-    version='0.3',
+    version='0.4.1',
     packages=['template_timings_panel', 'template_timings_panel.panels'],
     package_data={'': ['templates/*']},
     url='',
