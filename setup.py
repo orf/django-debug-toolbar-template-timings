376be23658ed4e7b7350b9af8d46364dc09fdc32
Orf <tom@tomforb.es>
2014-06-08 17:03:05 +0100

    Increment version

--- setup.py
+++ setup.py
@@ -7,10 +7,16 @@ if os.path.isfile("LICENSE"):
     with open('LICENSE') as f:
         license = f.read()
 
+readme = ""
+
+if os.path.isfile("README.rst"):
+    with open("README.rst") as f:
+        readme = f.read()
+
 
 setup(
     name='django-debug-toolbar-template-timings',
-    version='0.6.2',
+    version='0.6.3',
     packages=['template_timings_panel', 'template_timings_panel.panels'],
     package_data={'': ['templates/*']},
     url='https://github.com/orf/django-debug-toolbar-template-timings',
@@ -19,5 +25,5 @@ setup(
     author_email='tom@tomforb.es',
     description='A django-debug-toolbar panel that shows you template rendering times for Django',
     install_requires=['Django', 'django-debug-toolbar>=1.0'],
-    long_description=open('README.rst').read(),
+    long_description=readme,
 )
