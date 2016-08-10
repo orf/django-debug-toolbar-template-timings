d5f34de47311e5e0b0c6719d064be396683aed3d
Jean-Loup Adde <jean-loup.adde@airinov.fr>
2016-08-10 09:35:32 +0200

    Replacing the generic_tag_compiler to simple_tag for django 1.9 compatibility.

--- template_timings_panel/panels/TemplateTimings.py
+++ template_timings_panel/panels/TemplateTimings.py
@@ -1,6 +1,5 @@
 from debug_toolbar.panels import Panel
 from django.conf import settings
-from django.template import base as template_base
 from django.template import Template, Library
 from django.template.loader_tags import BlockNode
 from debug_toolbar.panels import sql
@@ -87,8 +86,7 @@ def wrap_generic_node(node, name):
             node.render = _template_render_wrapper(
                 node.render, node.__name__, name=lambda unused_: name)
 
-template_base.generic_tag_compiler = _tag_compiler(
-    template_base.generic_tag_compiler)
+Library.simple_tag = _tag_compiler(Library.simple_tag)
 
 
 def _template_render_wrapper(func, key, should_add=lambda n: True, name=lambda s: s.name if s.name else ''):
